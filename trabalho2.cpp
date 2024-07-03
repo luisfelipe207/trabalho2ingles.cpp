3a981f3fefcd377cb66f5d7de7e0aa116a19998a
luisfelipe207 <162385788+luisfelipe207@users.noreply.github.com>
2024-07-03 23:29:36 +0000

    trabalho 2

--- trabalho2.cpp
+++ trabalho2.cpp
@@ -2,7 +2,7 @@
 //PROF.: Glauber Kiss de Souza
 //DISC.: Analizar Orient. T�cnicas
 
-    // Defininda as varieaveis int
+    // Defining the int variables
 int soma = 13; 
 int carryBit = 0; 
 int nib1a,nib1b,nib1c,nib1d = 0;
@@ -11,7 +11,7 @@ int res1a,res1b,res1c,res1d = 0;
 
 void setup()
 {
-    // Configurando os pinos 0 a 7 como entradas e 8 a 12 como saídas
+    // Configuring pins 0 to 7 as inputs and 8 to 12 as outputs
     pinMode(0, INPUT);
     pinMode(1, INPUT);
     pinMode(2, INPUT);
@@ -30,7 +30,7 @@ void setup()
 
 int somaBit(int b1a, int b2a, int cBit)
 {
-    // Função para somar dois bits com o carryBit
+    // Function to add two bits with carryBit
     int bitResult = 0; 
     int aux1, aux2 = 0;
     if ((b1a ^ b2a) ^ cBit)
@@ -46,7 +46,7 @@ int somaBit(int b1a, int b2a, int cBit)
 
 int somaCarryBit(int b1a, int b2a, int cBit)
 {
-    // Função para calcular o carryBit
+// Function to calculate carryBit
     int aux1, aux2 = 0; 
     if ((b1a && b2a)||(b1a && cBit)||(b2a && cBit))
     {
@@ -61,7 +61,7 @@ int somaCarryBit(int b1a, int b2a, int cBit)
 
 void loop()
 {
-    // Função principal que executa continuamente
+    // Main function that runs continuously
     soma = digitalRead(13);
     nib1a = digitalRead(0);
     nib1b = digitalRead(1);
@@ -73,7 +73,7 @@ void loop()
     nib2d = digitalRead(7);
     if (soma == 1)
     {
-        carryBit = 0; // Inicializa carryBit como 0
+        carryBit = 0; 
         res1a = somaBit(nib1a,nib2a,carryBit);
         carryBit = somaCarryBit(nib1a,nib2a,carryBit);
         res1b = somaBit(nib1b,nib2b,carryBit);
@@ -83,7 +83,7 @@ void loop()
         res1d = somaBit(nib1d,nib2d,carryBit);
         carryBit = somaCarryBit(nib1d,nib2d,carryBit);
     }
-    // Escreve os resultados nos pinos de saída correspondentes
+    // Write results to the corresponding output pins
     digitalWrite(8,res1a);
     digitalWrite(9,res1b);
     digitalWrite(10,res1c);
